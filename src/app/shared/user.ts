fc3a32362105d88db5d355df69fc3e4ff4caaf95
pashok <vo100193ipa@gmail.com>
2017-06-04 13:25:28 +0300

    user...

--- src/app/shared/user.ts
+++ src/app/shared/user.ts
@@ -2,10 +2,4 @@ export class User {
     public id: string;
     public username: string;
     public password: string;
-
-    constructor(username, id) {
-        this.username = username;
-        this.id = id;
-
-    }
 }
