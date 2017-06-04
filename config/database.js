c1a97ac1d02053e5f4ed2fd16645778df7a9162b
pashok <vo100193ipa@gmail.com>
2017-06-04 13:43:34 +0300

    mdm

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: MONGODB_URI,    //dev
+  database: "mongodb://heroku_5qth300p:tp850jvbse73kve7d7v2emgc12@ds161901.mlab.com:61901/heroku_5qth300p",    
   secret: 'yoursecret'
 }
