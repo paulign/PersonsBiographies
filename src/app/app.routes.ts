cd794e89ec2b40abd4377d85814892938ca2dafc
pashok <vo100193ipa@gmail.com>
2017-08-12 17:12:33 +0300

    bag!!!!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from "@angular/router";
-import { PersonsListComponent, PersonBiographyComponent, LoginComponent, SignUpComponent, AdminComponent, ManagePersonsComponent, HomeComponent, LoginGuard } from "./index";
-
+import { PersonsListComponent } from './persons-list/persons-list.component';
+import { PersonBiographyComponent } from './person-biography/person-biography.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component'
+import { AdminComponent } from './admin/admin.component';
+import { ManagePersonsComponent } from './admin/manage-persons/manage-persons.component';
+import { HomeComponent } from './home/home.component';
+import { LoginGuard } from './shared/login-guard.service';
 
 export const routes: Routes = [
     {
